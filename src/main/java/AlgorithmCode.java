ef99a6ba6d73fe4b7e3062c70e10b0ae071113aa
kwamea236 <kwamea236@protonmail.com>
2023-10-25 09:02:26 +0000

    Add javadoc comment to algorithm implementation

--- src/main/java/AlgorithmCode.java
+++ src/main/java/AlgorithmCode.java
@@ -1,4 +1,18 @@
+/**
+ * Author: Raymond Ghanney Ato
+ * Date: 23-10-23
+ * Subject: Data Structures and Algorithm implementation in java.
+ * */
+
+
 public class AlgorithmCode {
+
+    /**
+     * This function is the implementation of the
+     * Linear search algorithm
+     * @param arr An array of items
+     * @param number item to be searched
+     * */
     public boolean linear_search(int[] arr, int number){
 
         for(int i =0; i < arr.length; i++){
